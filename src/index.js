c9d91fe21339b6b33c0e4d5a0fcd5051d0ce51ac
Aurelien Chaillot <chaillot.aurelien@gmail.com>
2021-04-22 11:20:19 +0200

    GraphQL quete 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/client/react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+const EXCHANGE_RATES = gql`
+    query GetExchangeRates {
+        launches(limit: 5) {
+            launch_date_utc
+            launch_success
+            rocket {
+                rocket_name
+            }
+            links {
+                video_link
+            }
+            details
+        }
+    }
+`;
+
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data);
+
+  return data.launches.map(({ details, rocket }) => (
+    <div key={details}>
+      <p>
+        {rocket.rocket_name}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
+    return (
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    );
+  }
+  
+
+  render(
+    <ApolloProvider client={client}>
+      <App />
+      <ExchangeRates />
+    </ApolloProvider>,
+    document.getElementById('root'),
+  );
+
